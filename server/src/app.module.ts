fbb0c588b22d9e7a843e1ec23f335c394cc14042
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-07-24 17:13:21 +0300

    Add user and auth services with JWT support to AppModule

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,9 +5,14 @@ import {ConfigModule} from "@nestjs/config";
 import * as process from "process";
 import {MongooseModule} from "@nestjs/mongoose";
 import {DataFaker} from "./mock-data/data-faker";
-import {User, UserSchema} from "./model/user.model";
-import {Product, ProductSchema} from "./model/product.model";
-import {Order, OrderSchema} from "./model/order.model";
+import {User, UserSchema} from "./model/document/user.model";
+import {Product, ProductSchema} from "./model/document/product.model";
+import {Order, OrderSchema} from "./model/document/order.model";
+import {UserService} from "./service/user.service";
+import {UserController} from "./controller/user.controller";
+import {AuthController} from "./controller/auth.controller";
+import {AuthService} from "./service/auth.service";
+import {JwtModule} from "@nestjs/jwt";
 
 @Module({
   imports: [
@@ -18,8 +23,13 @@ import {Order, OrderSchema} from "./model/order.model";
           { name: Product.name, schema: ProductSchema },
           { name: User.name, schema: UserSchema },
       ]),
+      JwtModule.register({
+          global: true,
+          secret: process.env.SECRET,
+          signOptions: { expiresIn: '60m' },
+      }),
   ],
-  controllers: [ProductController],
-  providers: [ProductService, DataFaker],
+  controllers: [AuthController, UserController, ProductController],
+  providers: [AuthService, UserService, ProductService, DataFaker],
 })
 export class AppModule {}
