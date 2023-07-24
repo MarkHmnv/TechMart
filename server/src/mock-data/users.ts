65f1507f244569c817bb2da39dfd9c53551226db
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-07-24 17:09:47 +0300

    Replace role string with enums in createUser function

--- server/src/mock-data/users.ts
+++ server/src/mock-data/users.ts
@@ -1,6 +1,7 @@
 import * as bcrypt from 'bcrypt';
+import {Role} from "../model/enum/role.enum";
 
-async function createUser(name: string, email: string, role = "user") {
+async function createUser(name: string, email: string, role = Role.USER) {
     const passwordHash = await bcrypt.hash('password', 10);
     return {
         name: name,
@@ -12,7 +13,7 @@ async function createUser(name: string, email: string, role = "user") {
 
 export async function createUsers() {
     return await Promise.all([
-        createUser('Admin User', 'admin@gmail.com', "admin"),
+        createUser('Admin User', 'admin@gmail.com', Role.ADMIN),
         createUser('Mark Himonov', 'markhmnv@gmail.com'),
         createUser('John Doe', 'johndoe@gmail.com'),
         createUser('Elon Musk', 'elonmusk@gmail.com'),
