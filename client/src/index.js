8b67b28af15aa0d8ab9cc3b99122b2d62c564ce0
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-07-24 17:23:31 +0300

    Refactor index.js for clarity and consistency

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {RouterProvider} from "react-router-dom";
-import {router} from "./routes/router";
-import {Provider} from "react-redux";
-import {store} from "./redux/store";
+import ReactDOM from 'react-dom';
+import Router from './routes/Router';
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-      <Provider store={store}>
-          <RouterProvider router={router}/>
-      </Provider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <Router />
+        </Provider>
+    </React.StrictMode>
 );
