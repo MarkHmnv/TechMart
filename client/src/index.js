990bcd5cec5a19a2319ae877e27694490fcb15a5
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-07-25 21:42:33 +0300

    Add PayPal integration for payment processing

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import Router from './routes/Router';
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import {PayPalScriptProvider} from "@paypal/react-paypal-js";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <Router />
+            <PayPalScriptProvider>
+                <Router />
+            </PayPalScriptProvider>
         </Provider>
     </React.StrictMode>
 );
